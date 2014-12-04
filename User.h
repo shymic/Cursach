15f939fc17ffded1b91bfe23002b24fe2fccde0c
Stanislav <stanislav.berkovich@gmail.com>
2014-12-05 00:43:32 +0200

    my first commit =). Refactor user.h

--- User.h
+++ User.h
@@ -14,21 +14,15 @@ private:
 	
 
 public:
-	void setName(string newName)
-	{
-		name = newName;
-	}
-	string getName()
-	{
-		return name;
-	}
-	User(string _login, string _password, string _name)	{}
-	bool autorize(string _login, string _password ){}
-	string info(){}
-	void addNote(){}
-	void deleteNote(int noteID){}
-	static bool readData(){};
-	static bool writeData(){};
+	void setName(string newName){ name = newName; }
+	string getName() { return name;	}
+	User(string _login, string _password, string _name);
+	bool autorize(string _login, string _password );
+	string info();
+	void addNote();
+	void deleteNote(int noteID);
+	static bool readData();
+	static bool writeData();
 	~User(){}
 
 };
