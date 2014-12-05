445add3d1169219615607b3845b53a624c069714
shymic <andreyshymko@gmail.com>
2014-12-06 00:38:43 +0300

    creatr user.h

---
+++ User.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+class User
+{
+private:
+	int ID;
+	string login;
+	string name;
+	string criptPassword;
+	string role;
+	static int nextID;
+	static string dataFileName;
+	
+
+public:
+	void setName(string newName)
+	{
+		name = newName;
+	}
+	string getName()
+	{
+		return name;
+	}
+	User(string _login, string _password, string _name)	{}
+	bool autorize(string _login, string _password ){}
+	string info(){}
+	void addNote(){}
+	void deleteNote(int noteID){}
+	static bool readData(){};
+	static bool writeData(){};
+	~User(){}
+
+};
