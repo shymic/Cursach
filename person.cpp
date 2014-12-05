f105805551fedd60042069623963b5a5b124914f
maxmilan <max231194@gmail.com>
2014-12-06 01:26:58 +0300

    Add realization of person

--- person.cpp
+++ person.cpp
@@ -1,3 +1,19 @@
 #pragma once
 #include "person.h"
 
+person::person(string _login, string _password, string _name)
+{
+	login = _login;
+	criptPassword = encrypt(_password);
+	name = _name;
+}
+
+person static person::autorize(string _login, string _password )
+{
+}
+
+string encrypt(string password)
+{
+	//TODO complete encryption of password
+	return password;
+}
